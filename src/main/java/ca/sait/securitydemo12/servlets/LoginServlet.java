dbbe3c0d208382401615722b48cebffdb9a89728
alexanderyee06 <alexander_yee06@hotmail.com>
2022-04-04 22:01:04 -0600

    Created the filter functionality to only allow access to admins

--- src/main/java/ca/sait/securitydemo12/servlets/LoginServlet.java
+++ src/main/java/ca/sait/securitydemo12/servlets/LoginServlet.java
@@ -40,6 +40,7 @@ public class LoginServlet extends HttpServlet {
         }
         
         HttpSession session = request.getSession();
+        session.setAttribute("user", user);
         session.setAttribute("email", email);
         
         if (user.getRole().getRoleId() == 1) {
--- target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
+++ target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-04T21:12:52")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-04T21:58:58")
 @StaticMetamodel(User.class)
 public class User_ { 
 
